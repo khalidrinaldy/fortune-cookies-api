87edc67ee35112af9fbf1d6b5a42c42a5cf94cd8
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-15 12:48:50 +0700

    fix port

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fortune-cookies/config"
+	//"fortune-cookies/config"
 	"fortune-cookies/routes"
+	"os"
+
 	"github.com/labstack/echo/middleware"
 
 	"github.com/labstack/echo"
@@ -15,10 +17,10 @@ func main() {
 	routes.InitRoute(ech)
 
 	//Config
-	cfg, _ := config.NewConfig(".env")
+	//cfg, _ := config.NewConfig(".env")
 
 	//use CORS
 	ech.Use(middleware.CORS())
 	
-	ech.Logger.Fatal(ech.Start(":" + cfg.Port))
+	ech.Logger.Fatal(ech.Start(":" + os.Getenv("PORT")))
 }

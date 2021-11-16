e6e981284d57ef53f6a44711c3e9a3cec90e75d3
khalidrinaldy <alid.rinaldy@gmail.com>
2021-11-16 18:04:58 +0700

    fix error

--- repository/cart_repository.go
+++ repository/cart_repository.go
@@ -57,7 +57,8 @@ func UpdateItemCart(db *gorm.DB) echo.HandlerFunc {
 func DeleteItemCart(db *gorm.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var deleteItem entity.Cart_Products
-		result := db.Exec("DELETE FROM cart_products WHERE cart_id = ? AND product_id = ?", c.Param("id"), c.FormValue("product_id"))
+		product_id,_ := strconv.Atoi(c.FormValue("product_id"))
+		result := db.Exec("DELETE FROM cart_products WHERE cart_id = ? AND product_id = ?", c.Param("id"), product_id)
 		if result.Error != nil {
 			return c.JSON(http.StatusOK, helper.ResultResponse(true, "Delete Item Cart Failed", result.Error))
 		}

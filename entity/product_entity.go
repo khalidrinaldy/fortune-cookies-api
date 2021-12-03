2097c7c2ecc89558ff12bfb3aa1e0d3a5076967c
khalidrinaldy <alid.rinaldy@gmail.com>
2021-12-03 12:56:38 +0700

    fix error

--- entity/product_entity.go
+++ entity/product_entity.go
@@ -1,7 +1,7 @@
 package entity
 
 type Product struct {
-	Id                  int `gorm:"primarykey"`
+	ID                  int `gorm:"primarykey"`
 	Product_Name        string `json:"product_name"`
 	Product_Price       int    `json:"product_price"`
 	Product_Category    string `json:"product_category"`
--- repository/product_repository.go
+++ repository/product_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"fortune-cookies/entity"
 	"fortune-cookies/helper"
 	"net/http"
@@ -96,31 +97,29 @@ func UpdateProduct(db *gorm.DB) echo.HandlerFunc {
 			return c.JSON(http.StatusOK, helper.ResultResponse(true, "Admin Token Not Found", ""))
 		}
 
-		//product.ID,_ = strconv.Atoi(c.Param("id"))
+		product.ID,_ = strconv.Atoi(c.Param("product_id"))
 		product.Product_Name = c.FormValue("product_name")
 		product.Product_Category = c.FormValue("product_category")
 		product.Product_Price, _ = strconv.Atoi(c.FormValue("product_price"))
 		product.Product_Image = c.FormValue("product_image")
 		product.Product_Description = c.FormValue("product_description")
 
-		result := db.Model(&product).Where("id = ?", c.Param("product_id")).Updates(map[string]interface{}{
-			"product_name":        product.Product_Name,
-			"product_category":    product.Product_Category,
-			"product_price":       product.Product_Price,
-			"product_image":       product.Product_Image,
-			"product_description": product.Product_Description,
-		})
-		// result := db.Exec(fmt.Sprintf(`
-		// 	update products
-		// 	set product_name = '%s', product_category = '%s', product_price = %d, product_image = '%s', product_description = '%s'
-		// 	where id = %d;`,
-		// 	product.Product_Name,
-		// 	product.Product_Category,
-		// 	product.Product_Price,
-		// 	product.Product_Image,
-		// 	product.Product_Description,
-		// 	product.ID,
-		// ))
+		// result := db.Model(&product).Where("id = ?", c.Param("product_id")).Updates(map[string]interface{}{
+		// 	"product_name":        product.Product_Name,
+		// 	"product_category":    product.Product_Category,
+		// 	"product_price":       product.Product_Price,
+		// 	"product_image":       product.Product_Image,
+		// 	"product_description": product.Product_Description,
+		// })
+		query := fmt.Sprintf(`update products set product_name = '%s', product_category = '%s', product_price = %d, product_image = '%s', product_description = '%s' where id = %d;`,
+		product.Product_Name,
+		product.Product_Category,
+		product.Product_Price,
+		product.Product_Image,
+		product.Product_Description,
+		product.ID,
+	)
+		result := db.Exec(query)
 		if result.Error != nil {
 			return c.JSON(http.StatusOK, helper.ResultResponse(true, c.Param("product_id"), result.Error.Error()))
 		}

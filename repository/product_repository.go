6458679d248d4b1d602dd608dd349c06da3a88d6
khalidrinaldy <alid.rinaldy@gmail.com>
2021-12-03 12:31:03 +0700

    fix error

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"fmt"
 	"fortune-cookies/entity"
 	"fortune-cookies/helper"
 	"net/http"
@@ -97,31 +96,31 @@ func UpdateProduct(db *gorm.DB) echo.HandlerFunc {
 			return c.JSON(http.StatusOK, helper.ResultResponse(true, "Admin Token Not Found", ""))
 		}
 
-		product.ID,_ = strconv.Atoi(c.Param("id"))
+		//product.ID,_ = strconv.Atoi(c.Param("id"))
 		product.Product_Name = c.FormValue("product_name")
 		product.Product_Category = c.FormValue("product_category")
 		product.Product_Price, _ = strconv.Atoi(c.FormValue("product_price"))
 		product.Product_Image = c.FormValue("product_image")
 		product.Product_Description = c.FormValue("product_description")
 
-		// result := db.Model(&product).Where("id = ?", c.Param("id")).Updates(map[string]interface{}{
-		// 	"product_name":     product.Product_Name,
-		// 	"product_category": product.Product_Category,
-		// 	//"product_price":    product.Product_Price,
-		// 	"product_image":    product.Product_Image,
-		// 	"product_description": product.Product_Description,
-		// })
-		result := db.Exec(fmt.Sprintf(`
-			update products
-			set product_name = '%s', product_category = '%s', product_price = %d, product_image = '%s', product_description = '%s'
-			where id = %d;`, 
-			product.Product_Name,
-			product.Product_Category,
-			product.Product_Price,
-			product.Product_Image,
-			product.Product_Description,
-			product.ID,
-		))
+		result := db.Model(&product).Where("id = ?", c.Param("id")).Updates(map[string]interface{}{
+			"product_name":        product.Product_Name,
+			"product_category":    product.Product_Category,
+			"product_price":       product.Product_Price,
+			"product_image":       product.Product_Image,
+			"product_description": product.Product_Description,
+		})
+		// result := db.Exec(fmt.Sprintf(`
+		// 	update products
+		// 	set product_name = '%s', product_category = '%s', product_price = %d, product_image = '%s', product_description = '%s'
+		// 	where id = %d;`,
+		// 	product.Product_Name,
+		// 	product.Product_Category,
+		// 	product.Product_Price,
+		// 	product.Product_Image,
+		// 	product.Product_Description,
+		// 	product.ID,
+		// ))
 		if result.Error != nil {
 			return c.JSON(http.StatusOK, helper.ResultResponse(true, "Error Occured While Querying SQL", result.Error.Error()))
 		}
